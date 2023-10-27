c4bb1f5aabfa0c1c7024d9a2868ea3543ae5d831
diefesson <diefesson.so@gmail.com>
2023-10-27 16:45:54 -0300

    Impl movimento do jogador

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int Life;
+    public int MaxLife;
+    public float speed;
+
+}
