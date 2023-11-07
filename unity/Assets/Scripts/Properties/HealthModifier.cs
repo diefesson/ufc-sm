2148e0ed98c3ffed46079243174710dbc27fc2c0
diefesson <diefesson.so@gmail.com>
2023-11-07 20:49:47 -0300

    Impl bala

--- unity/Assets/Scripts/Properties/HealthModifier.cs
+++ unity/Assets/Scripts/Properties/HealthModifier.cs
@@ -8,9 +8,9 @@ public class HealthModifier : MonoBehaviour
     public bool affectsPlayer = true;
     public bool affectsEnemy = false;
 
-    private void OnTriggerEnter2D(Collider2D collision) => ProcessCollision(collision.gameObject);
+    void OnTriggerEnter2D(Collider2D collision) => ProcessCollision(collision.gameObject);
 
-    private void OnCollisionEnter2D(Collision2D collision) => ProcessCollision(collision.gameObject);
+    void OnCollisionEnter2D(Collision2D collision) => ProcessCollision(collision.gameObject);
 
     private void ProcessCollision(GameObject other)
     {
@@ -19,9 +19,7 @@ public class HealthModifier : MonoBehaviour
         var isEnemy = other.GetComponent<Enemy>() != null;
         if (healthProp != null && (affectsPlayer && isPlayer || affectsEnemy && isEnemy))
         {
-            print("1 " + healthProp.Health);
             healthProp.Health += change;
-            print("2 " + healthProp.Health);
             if (selfDestruction)
             {
                 Destroy(gameObject);
