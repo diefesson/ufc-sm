b18b767a50295f8bba4080037f754faa1e6ac07e
diefesson <diefesson.so@gmail.com>
2023-11-08 17:34:49 -0300

    Prefab para Camera e Background

--- unity/Assets/Scripts/Misc/FollowerCamera.cs
+++ unity/Assets/Scripts/Misc/FollowerCamera.cs
@@ -1,27 +1,23 @@
+using System.Linq;
 using UnityEngine;
 
 public class FollowerCamera : MonoBehaviour
 {
 
-    public GameObject target;
+    public float lookahead = 1;
 
-    public float lookahead;
-
-    public float smoothing;
-
-    private Rigidbody2D targetRb;
-
-    void Start()
-    {
-        targetRb = target.GetComponent<Rigidbody2D>();
-    }
+    public float smoothing = 0.1f;
 
     void FixedUpdate()
     {
+        var target = Player.Players.FirstOrDefault();
+        if (target == null)
+            return;
+        var targetRb = target.GetComponent<Rigidbody2D>();
         var oldPosition = new Vector2(transform.position.x, transform.position.y);
         var targetPosition = targetRb.position;
         var targetVelocity = targetRb.velocity;
-        var lookaheadPosition = targetPosition + lookahead * targetVelocity;
+        var lookaheadPosition = targetPosition + lookahead * targetVelocity.normalized;
         var smoothPosition = Vector2.Lerp(oldPosition, lookaheadPosition, smoothing);
         transform.position = new Vector3(smoothPosition.x, smoothPosition.y, transform.position.z);
     }
